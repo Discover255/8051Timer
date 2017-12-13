6ce6476f6bef89777d73a2a0c6959758420cc649
兰陵笑笑生 <1215223072@qq.com>
2017-12-13 08:38:40 +0800

    暂时没有实现启停，存在BUG
    暂时没有实现启停，有BUG,回滚到之前版本发现依然编译的是有BUG的那个版本，明天再继续

--- src/main.asm
+++ src/main.asm
@@ -25,7 +25,7 @@ MAIN: 	MOV  R0,#0
 		LJMP IF0
 IF0:	CJNE R7,#255,LOOP1
 		MOV  R7,#0
-		CALL TIMEDEC
+		LCALL TIMEDEC
 		CPL	 P1.0
 LOOP1:	NOP
 IF2:	CJNE R0,#0,ELIF21
@@ -59,6 +59,10 @@ BREAK1:	MOV  DPTR,#SEGTAB
 		JB   00H,IF15TRUNK;sometimes flagbit read is wrong,00H is a signal with a low freq for  flick
 		LCALL CHECKP30
 		LCALL CHECKP31
+		LCALL CHECKP32
+		JNB  09H,IF43
+		CLR  09H
+		LCALL MOVSELECT
 IF43:	JNB  06H,IF21
 		CLR  06H
 		LCALL TIMEINC
@@ -91,6 +95,26 @@ RESET1: MOV  R0,#0
 		LCALL INDEX
 IF10:	CLR  RS1
 		LJMP IF0
+
+;========================================================================================
+
+INT:	NOP
+		MOV  TL0,#1
+		MOV  TH0,#240
+		JNB  0DH,IF42
+		INC  07H;provide a clock for timer
+IF42:	INC  10H;increase group 3th R0,for update of view
+		INC  11H;increase group 3th R1,for flick of time
+IF30:	JNB  02H,IF31;check P3.0 keyup event
+		INC  12H;increase group 3th R2,for confirm the event of P3.0 keyup
+IF31:	JNB  05H,IF47;check P3.1 keyup event
+		INC  14H;increase group 3th R4,for confirm the event of P3.1 keyup
+IF47:	JNB  08H,QUITINT;check P3.2 keyup event
+		INC  15H;increase group 3th R5,for confirm the event of P3.2 keyup
+QUITINT:RETI
+
+;===================================================================================================
+
 TIMEDEC:NOP
 IF3:	CJNE R6,#0,ELSE3
 IF4:	CJNE R5,#0,ELSE4
@@ -99,6 +123,7 @@ IF6:	CJNE R3,#0,ELSE6
 IF7:	CJNE R2,#0,ELSE7
 IF8:	CJNE R1,#0,ELSE8
 		CLR  P2.3
+		CLR  09H
 		RET
 ELSE8:	DEC	 R1
 		LJMP BORROW2
@@ -119,22 +144,7 @@ BORROW5:MOV  R5,#5
 BORROW6:MOV  R6,#9
 		RET
 
-;========================================================================================
-
-INT:	NOP
-		MOV  TL0,#1
-		MOV  TH0,#240
-		JNB  0DH,IF42
-		INC  07H;provide a clock for timer
-IF42:	INC  10H;increase group 3th R0,for update of view
-		INC  11H;increase group 3th R1,for flick of time
-IF30:	JNB  02H,IF31;check P3.0 keyup event
-		INC  12H;increase group 3th R2,for confirm the event of P3.0 keyup
-IF31:	JNB  05H,QUITINT;check P3.1 keyup event
-		INC  14H;increase group 3th R3,for confirm the event of P3.1 keyup
-QUITINT:RETI
-
-;===================================================================================================
+;-------------------------------------------------------------------------------------------
 
 INDEX:  INC  R0
 		MOV  A,#6
@@ -228,7 +238,7 @@ RETURN6:	RET
 CHECKP31:	JB   P3.1,BREAK26
 			SETB 04H
 	BREAK26:JNB  04H,BREAK27
-			JNB  P3.0,BREAK27
+			JNB  P3.1,BREAK27
 			SETB 05H
 	BREAK27:MOV  A,14H
 			CJNE A,#5,ELSE28
@@ -259,8 +269,42 @@ RETURN7:	RET
 
 ;---------------------------------------------------------------------------------------------
 
+CHECKP32:	JB   P3.2,BREAK44
+			SETB 07H
+	BREAK44:JNB  07H,BREAK45
+			JNB  P3.2,BREAK45
+			SETB 08H
+	BREAK45:MOV  A,15H
+			CJNE A,#5,ELSE46
+			MOV  14H,#0
+			CLR  07H
+			CLR  08H
+			JNB  P3.2,KEYUP21
+			CLR  08H
+			CPL  P1.5
+			SETB 06H
+			LJMP BREAK46
+	KEYUP21:SETB 08H
+			LJMP BREAK46
+	ELSE46: JC   BREAK46
+			MOV  14H,#0
+			CLR  07H
+			CLR  08H
+			JNB  P3.2,KEYUP22
+			CLR  08H
+			CPL  P1.5
+			SETB 06H
+			LJMP BREAK46
+	KEYUP22:SETB 08H
+			LJMP BREAK46
+	BREAK46:JB   08H,RETURN9
+			CLR  08H
+RETURN9:	RET
+
+;---------------------------------------------------------------------------------------------
+
 TIMEINC:	MOV  A,13H
-			CLR  RS1
+			;CLR  RS1
 IF35:		CJNE A,#1,ELIF35_1
 			LJMP INCH
 ELIF35_1:	CJNE A,#2,ELIF35_2
@@ -270,36 +314,43 @@ ELIF35_2: 	CJNE A,#3,RETURN8
 RETURN8:	RET
 
 INCH:		NOP
-IF36:		CJNE R2,#9,ELSE36;addressing R2,R1 using name may cause wrong addressing (RS0 == 0,RS1 == 0)
-IF37:		CJNE R1,#9,ELSE37
-			MOV  R1,#0
-			MOV  R2,#0
-ELSE37:		INC  R1
-			MOV  R2,#0
+IF36:		MOV  A,02H
+			CJNE A,#9,ELSE36;
+			;CJNE R2,#9,ELSE36;addressing R2,R1 using name may cause wrong addressing (RS0 == 0,RS1 == 0)
+IF37:		MOV  A,01H
+			CJNE A,#9,ELSE37
+			MOV  01H,#0
+			MOV  02H,#0
+ELSE37:		INC  01H
+			MOV  02H,#0
 			RET
-ELSE36:		INC  R2
+ELSE36:		INC  02H
 			RET
 
 INCM:		NOP
-IF38:		CJNE R4,#9,ELSE38
-IF39:		CJNE R3,#5,ELSE39
-			MOV  R3,#0
-			MOV  R4,#0
-ELSE39:		INC  R3
-			MOV  R4,#0
+IF38:		MOV  A,04H
+			CJNE A,#9,ELSE38
+IF39:		MOV  A,03H
+			CJNE A,#5,ELSE39
+			MOV  03H,#0
+			MOV  04H,#0
+ELSE39:		INC  03H
+			MOV  04H,#0
 			RET
-ELSE38:		INC  R4
+ELSE38:		INC  04H
 			RET
 
 INCS:		NOP
-IF40:		CJNE R6,#9,ELSE40
-IF41:		CJNE R5,#5,ELSE41
-			MOV  R5,#0
-			MOV  R6,#0
-ELSE41:		INC  R5
-			MOV  R6,#0
+IF40:		MOV  A,06H
+			CJNE A,#9,ELSE40
+IF41:		MOV  A,05H
+			CJNE A,#5,ELSE41
+			MOV  05H,#0
+			MOV  06H,#0
+ELSE41:		INC  05H
+			MOV  06H,#0
 			RET
-ELSE40:		INC  R6
+ELSE40:		INC  06H
 			RET
 ;---------------------------------------------------------------------------------------------
 
